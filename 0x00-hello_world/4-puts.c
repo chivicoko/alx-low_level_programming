d4e0e869cc22cc6dea479a38246441703cad24a6
chivicoko <victor.c.okoye@gmail.com>
2023-03-16 13:23:14 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,14 +4,16 @@
 
 /**
  * main - prints "Programming is like building a multilingual
- * puzzle, followed by a newline."
- * return 0.
+ * puzzle, followed by a newline.
+ * Return: Always 0.
  */
+
 
 int main(void)
 
+
 {
         puts("\"Programming is like building a multilingual puzzle");
-
         return (0);
+
 }
