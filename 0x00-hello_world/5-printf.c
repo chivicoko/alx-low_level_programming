ca793dcc21c0256130b1d6e418bd5353ab3d8deb
chivicoko <victor.c.okoye@gmail.com>
2023-03-16 13:00:31 -0700

    5...

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+
 /**
  * main - prints "with proper grammar, but the outcome is
  * a piece of art," followed newline
@@ -8,6 +9,8 @@
  */
 
 int main(void)
+
+
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
