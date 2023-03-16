d48b7f545b3c1d7f62f9e504ccb4f0d75fdf7d98
chivicoko <victor.c.okoye@gmail.com>
2023-03-16 13:17:29 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,17 @@
-#include<stdio.h>
+#include <stdio.h>
+
+
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - prints "Programming is like building a multilingual
+ * puzzle, followed by a newline."
+ * return 0.
  */
+
 int main(void)
+
 {
-	puts("\"Programming is like building a multiple puzzle");
-	return (0);
+        puts("\"Programming is like building a multilingual puzzle");
+
+        return (0);
 }
