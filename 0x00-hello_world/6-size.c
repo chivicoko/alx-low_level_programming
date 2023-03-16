6bed267fd5d03d9ef9b5188f3594ff10577ba46f
chivicoko <victor.c.okoye@gmail.com>
2023-03-16 13:51:45 -0700

    6...

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 
 
 /**
- * main - Prints the size of various types based on
+ * main - Prints the size of various typesa based on
  * the computer it is compiled and run on..
  * Return: Always 0.
  */
@@ -11,10 +11,10 @@ int main(void)
 
 {
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
-	printf("Size of a int: %zu byte(s)\n", sizeof(int));
+
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %zu byte(s)\n", sizeof(float));
-
 	return (0);
 }
