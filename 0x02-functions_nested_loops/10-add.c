24bf811a6359f7404e28385dbdfc46eafc93b424
chivicoko <victor.c.okoye@gmail.com>
2023-03-21 12:58:18 -0700

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - function to add two numbers
+ *
+ * @a: parameter 1;
+ * @b: pparamter 2;
+ *
+ * Return: sum;
+ */ 
+
+int add(int a, int b)
+{
+	return (a + b);
+}
